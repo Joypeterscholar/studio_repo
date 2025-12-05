2b140f10e7841030e5012f877a231e6050418035
joy scholar <joyscholar077@gmail.com>
2025-12-05 23:51:41 +0000

    DEVELOP THIS SCREEN

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default function HomePage() {
     const router = useRouter();
 
     useEffect(() => {
-        router.replace('/password-reset-code');
+        router.replace('/new-password');
     }, [router]);
 
     return (
         <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
-            <p>Redirecting to password reset code page...</p>
+            <p>Redirecting to new password page...</p>
         </div>
     );
 }
