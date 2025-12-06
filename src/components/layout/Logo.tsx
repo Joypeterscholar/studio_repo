bf3d9cac359e9d10c77d0ee4295562c5880400e2
joy scholar <joyscholar077@gmail.com>
2025-12-06 07:00:46 +0000

    SEE D LOGOO...IT SHOULD LOOK LIKE THE SPLASH SCREEN I GAVE

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -9,33 +9,19 @@ export default function Logo({ className, isLinqUp = false }: LogoProps) {
   if (isLinqUp) {
     return (
       <svg
-        viewBox="0 0 290 60"
+        viewBox="0 0 285 50"
         xmlns="http://www.w3.org/2000/svg"
         className={cn("text-primary-foreground", className)}
-        style={{ letterSpacing: '0.05em' }}
       >
-        <defs>
-          <path id="q-outer" d="M20,1.5c10.23,0,18.5,8.27,18.5,18.5S30.23,38.5,20,38.5S1.5,30.23,1.5,20S9.77,1.5,20,1.5z" />
-          <path id="q-inner" d="M20,6.85c7.26,0,13.15,5.89,13.15,13.15S27.26,33.15,20,33.15S6.85,27.26,6.85,20S12.74,6.85,20,6.85z" />
-          <path id="female-symbol" d="M14.6,18.05v-5.4h2.7v-2.7h-2.7v-2.7h-2.7v2.7h-2.7v2.7h2.7v5.4c0,2.98,2.42,5.4,5.4,5.4h2.7v2.7h-2.7c-4.49,0-8.1-3.61-8.1-8.1Z" />
-          <path id="male-symbol" d="m30.7,11.15l-4.05,4.05,2.7,2.7-4.05,4.05,1.89,1.89,4.05-4.05,2.7,2.7v-8.1Z" />
-          <path id="refresh-arrow" d="M21.95,6.85c-1.49,0-2.86,0.59-3.89,1.62l-2.7-2.7v8.1h8.1l-2.7-2.7c0.5-0.5,1.17-0.81,1.89-0.81,1.49,0,2.7,1.22,2.7,2.7s-1.22,2.7-2.7,2.7h-5.4v2.7h5.4c2.98,0,5.4-2.42,5.4-5.4s-2.42-5.4-5.4-5.4Z" />
-        </defs>
-        
-        <text fontFamily="Alegreya Sans SC, sans-serif" fontWeight="900" fontSize="48" fill="currentColor">
-          <tspan x="0" y="45">LIN</tspan>
-          <tspan x="200" y="45">UP</tspan>
+        <text 
+            fontFamily="'Alegreya Sans SC', sans-serif" 
+            fontWeight="900" 
+            fontSize="48" 
+            fill="currentColor"
+            className="linqup-text"
+        >
+            <tspan x="0" y="45">LINQUP</tspan>
         </text>
-
-        <g transform="translate(115, 10)" fill="hsl(var(--accent))">
-          <use href="#q-outer" stroke="hsl(var(--accent))" strokeWidth="3" fill="none"/>
-          <use href="#q-inner" fill="hsl(var(--primary))"/>
-          <g fill="hsl(var(--accent))">
-            <use href="#female-symbol"/>
-            <use href="#male-symbol"/>
-            <use href="#refresh-arrow"/>
-          </g>
-        </g>
       </svg>
     );
   }
