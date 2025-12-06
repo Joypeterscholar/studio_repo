fedb35da2a809e05c8407a8ef8c88eb068754867
joy scholar <joyscholar077@gmail.com>
2025-12-06 06:58:15 +0000

    mostly the logo.....u no get am....see d logo

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -10,7 +10,7 @@ export default function Logo({ className, isLinqUp = false }: LogoProps) {
   if (isLinqUp) {
     return (
         <svg
-            viewBox="0 0 240 50"
+            viewBox="0 0 250 50"
             xmlns="http://www.w3.org/2000/svg"
             className={cn("text-primary-foreground", className)}
         >
@@ -29,24 +29,7 @@ export default function Logo({ className, isLinqUp = false }: LogoProps) {
           y="35"
           className="linqup-text"
         >
-          LIN
-        </text>
-        <g transform="translate(105, 0)">
-            <path
-                d="M25,1.75a23.25,23.25,0,1,0,23.25,23.25A23.25,23.25,0,0,0,25,1.75Zm-4.37,21.56a1.5,1.5,0,0,1-2.12,0l-3.54-3.54a1.5,1.5,0,0,1,2.12-2.12l3.54,3.54a1.5,1.5,0,0,1,0,2.12Zm5.44-15.65,1.13,3.38,3.38,1.13a1.7,1.7,0,0,1,0,3.18l-3.38,1.13-1.13,3.38a1.7,1.7,0,0,1-3.18,0l-1.13-3.38-3.38-1.13a1.7,1.7,0,0,1,0-3.18l3.38-1.13,1.13-3.38a1.7,1.7,0,0,1,3.18,0Zm9,12.11a1.5,1.5,0,0,1,0-2.12l3.54-3.54a1.5,1.5,0,0,1,2.12,2.12l-3.54,3.54a1.5,1.5,0,0,1-2.12,0Z"
-                fill="hsl(var(--accent))"
-            />
-            <path
-                d="M17.47,20.69a1.5,1.5,0,0,1,2.12,0l3.54,3.54a1.5,1.5,0,0,1-2.12,2.12l-3.54-3.54a1.5,1.5,0,0,1,0-2.12Z"
-                fill="hsl(var(--accent))"
-            />
-        </g>
-        <text
-          x="165"
-          y="35"
-          className="linqup-text"
-        >
-          UP
+          LINQUP
         </text>
       </svg>
     );
