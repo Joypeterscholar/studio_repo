55bda11b6d70ded2e8f343789c301d52c226330b
joy scholar <joyscholar077@gmail.com>
2025-12-11 06:29:07 +0000

    im ready

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,11 @@
 // This file is intentionally left blank.
 // The Firebase configuration will be injected by the build process.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+    apiKey: "AIzaSyC2XPDrv6i2uwfO6TIboIsFdrJulTs6SQ4",
+    authDomain: "linqupweb.firebaseapp.com",
+    projectId: "linqupweb",
+    storageBucket: "linqupweb.firebasestorage.app",
+    messagingSenderId: "254862196833",
+    appId: "1:254862196833:web:53e0372a14feb20ff42a20",
+    measurementId: "G-DCTD3G2476"
+  };
